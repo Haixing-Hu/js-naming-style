4c357cb2e83be20a381ebed9f5d60a61447977d4
Haixing Hu <starfish.hu@gmail.com>
2023-12-08 11:54:10 +0800

    build: upgrade @haixing_hu/rollup-builder and fix the compatibility issue of building output

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,4 +8,4 @@
 ////////////////////////////////////////////////////////////////////////////////
 import rollupBuilder from '@haixing_hu/rollup-builder';
 
-export default rollupBuilder('NamingStyle', import.meta.url);
+export default rollupBuilder('NamingStyle', import.meta.url, { exports: 'mixed' });
